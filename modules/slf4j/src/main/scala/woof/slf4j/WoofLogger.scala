396e18bbe949883f45e3a5515931f861d593ae74
Felix <felix.palludan.hargreaves@lego.com>
2021-10-29 12:52:24 +0200

    Ignore markers for now.

--- modules/slf4j/src/main/scala/woof/slf4j/WoofLogger.scala
+++ modules/slf4j/src/main/scala/woof/slf4j/WoofLogger.scala
@@ -19,19 +19,16 @@ class WoofLogger(name: String) extends Logger:
   private def getLogInfo() =
     val stacktraceElements = Thread.currentThread().getStackTrace()
     val callingMethodIndex =
-      stacktraceElements.indexWhere(s =>
-        val cm       = s.getMethodName
-        val cn       = s.getClassName
-        val isMethod = List("info", "trace", "warn", "debug", "error").contains(cm)
-        val isClass  = cn.equalsIgnoreCase("woof.slf4j.WoofLogger")
-        isClass && isMethod,
-      ) + 1 // go 1 up in the stack
+      stacktraceElements.size - stacktraceElements.reverse.indexWhere(s => s.getClassName == this.getClass.getName,
+      ) // after last mention of this class
     val callingMethod: StackTraceElement = stacktraceElements(callingMethodIndex)
     val className                        = callingMethod.getClassName
     val fileName                         = className.replace('.', '/') + ".scala"
     val lineNumber                       = callingMethod.getLineNumber - 1
     LogInfo(className, new File(fileName), lineNumber)
   end getLogInfo
+
+  def getName(): String = name
 
   private def log(level: LogLevel, msg: String) = logger.foreach(_.doLog(level, msg, getLogInfo()).unsafeRunSync())
   def info(msg: String): Unit                   = log(LogLevel.Info, msg)
@@ -40,69 +37,74 @@ class WoofLogger(name: String) extends Logger:
   def trace(msg: String): Unit                  = log(LogLevel.Trace, msg)
   def warn(msg: String): Unit                   = log(LogLevel.Warn, msg)
 
-  def debug(msg: String, obj: Object): Unit                        = debug(s"$msg, $obj")
-  def debug(msg: String, obj1: Object, obj2: Object): Unit         = debug(s"$msg, $obj1, $obj2")
-  def debug(msg: String, objs: Array[? <: Object]): Unit           = debug(s"$msg, ${objs.mkString(", ")}")
-  def debug(msg: String, throwable: Throwable): Unit               = debug(s"$msg, ${throwable.getMessage}")
-  def debug(x$0: org.slf4j.Marker, msg: String): Unit              = ???
-  def debug(x$0: org.slf4j.Marker, msg: String, x$2: Object): Unit = ???
-  def debug(x$0: org.slf4j.Marker, msg: String, x$2: Object, x$3: Object): Unit = ???
-  def debug(x$0: org.slf4j.Marker, msg: String, x$2: Array[? <: Object]): Unit  = ???
-  def debug(x$0: org.slf4j.Marker, msg: String, x$2: Throwable): Unit           = ???
+  def debug(msg: String, obj: Object): Unit                        = debug(s"$msg $obj")
+  def debug(msg: String, obj1: Object, obj2: Object): Unit         = debug(s"$msg $obj1, $obj2")
+  def debug(msg: String, objs: Array[? <: Object]): Unit           = debug(s"$msg ${objs.mkString(", ")}")
+  def debug(msg: String, throwable: Throwable): Unit               = debug(s"$msg ${throwable.getMessage}")
+  def debug(x$0: org.slf4j.Marker, msg: String): Unit              = debug(msg)
+  def debug(x$0: org.slf4j.Marker, msg: String, obj: Object): Unit = debug(s"$msg, $obj")
+  def debug(x$0: org.slf4j.Marker, msg: String, obj1: Object, obj2: Object): Unit = debug(s"$msg, $obj1, $obj2")
+  def debug(x$0: org.slf4j.Marker, msg: String, objs: Array[? <: Object]): Unit = debug(s"$msg ${objs.mkString(", ")}")
+  def debug(x$0: org.slf4j.Marker, msg: String, throwable: Throwable): Unit     = debug(s"$msg ${throwable.getMessage}")
 
-  def error(msg: String, obj: Object): Unit                        = error(s"$msg, $obj")
-  def error(msg: String, obj1: Object, obj2: Object): Unit         = error(s"$msg, $obj1, $obj2")
-  def error(msg: String, objs: Array[? <: Object]): Unit           = error(s"$msg, ${objs.mkString(", ")}")
-  def error(msg: String, throwable: Throwable): Unit               = error(s"$msg, ${throwable.getMessage}")
-  def error(x$0: org.slf4j.Marker, msg: String): Unit              = ???
-  def error(x$0: org.slf4j.Marker, msg: String, x$2: Object): Unit = ???
-  def error(x$0: org.slf4j.Marker, msg: String, x$2: Object, x$3: Object): Unit = ???
-  def error(x$0: org.slf4j.Marker, msg: String, x$2: Array[? <: Object]): Unit  = ???
-  def error(x$0: org.slf4j.Marker, msg: String, x$2: Throwable): Unit           = ???
-  def getName(): String                                                         = name
+  def error(msg: String, obj: Object): Unit                        = error(s"$msg $obj")
+  def error(msg: String, obj1: Object, obj2: Object): Unit         = error(s"$msg $obj1, $obj2")
+  def error(msg: String, objs: Array[? <: Object]): Unit           = error(s"$msg ${objs.mkString(", ")}")
+  def error(msg: String, throwable: Throwable): Unit               = error(s"$msg ${throwable.getMessage}")
+  def error(x$0: org.slf4j.Marker, msg: String): Unit              = error(msg)
+  def error(x$0: org.slf4j.Marker, msg: String, obj: Object): Unit = error(s"$msg $obj")
+  def error(x$0: org.slf4j.Marker, msg: String, obj1: Object, obj2: Object): Unit = error(s"$msg $obj1, $obj2")
+  def error(x$0: org.slf4j.Marker, msg: String, objs: Array[? <: Object]): Unit = error(s"$msg ${objs.mkString(", ")}")
+  def error(x$0: org.slf4j.Marker, msg: String, throwable: Throwable): Unit     = error(s"$msg ${throwable.getMessage}")
 
-  def info(msg: String, obj: Object): Unit                                     = info(s"$msg, $obj")
-  def info(msg: String, obj1: Object, obj2: Object): Unit                      = info(s"$msg, $obj1, $obj2")
-  def info(msg: String, objs: Array[? <: Object]): Unit                        = info(s"$msg, ${objs.mkString(", ")}")
-  def info(msg: String, throwable: Throwable): Unit                            = info(s"$msg, ${throwable.getMessage}")
-  def info(x$0: org.slf4j.Marker, msg: String): Unit                           = ???
-  def info(x$0: org.slf4j.Marker, msg: String, x$2: Object): Unit              = ???
-  def info(x$0: org.slf4j.Marker, msg: String, x$2: Object, x$3: Object): Unit = ???
-  def info(x$0: org.slf4j.Marker, msg: String, x$2: Array[? <: Object]): Unit  = ???
-  def info(x$0: org.slf4j.Marker, msg: String, x$2: Throwable): Unit           = ???
-
-  private def testLevel(logLevel: LogLevel): Boolean = logger.exists(_.filter(LogLine(logLevel, getLogInfo(), "", Nil)))
-  def isDebugEnabled(): Boolean                      = testLevel(LogLevel.Debug)
-  def isErrorEnabled(): Boolean                      = testLevel(LogLevel.Error)
-  def isInfoEnabled(): Boolean                       = testLevel(LogLevel.Info)
-  def isTraceEnabled(): Boolean                      = testLevel(LogLevel.Trace)
-  def isWarnEnabled(): Boolean                       = testLevel(LogLevel.Warn)
-
-  def isInfoEnabled(x$0: org.slf4j.Marker): Boolean  = ???
-  def isTraceEnabled(x$0: org.slf4j.Marker): Boolean = ???
-  def isDebugEnabled(x$0: org.slf4j.Marker): Boolean = ???
-  def isErrorEnabled(x$0: org.slf4j.Marker): Boolean = ???
-  def isWarnEnabled(x$0: org.slf4j.Marker): Boolean  = ???
+  def info(msg: String, obj: Object): Unit                                       = info(s"$msg, $obj")
+  def info(msg: String, obj1: Object, obj2: Object): Unit                        = info(s"$msg, $obj1, $obj2")
+  def info(msg: String, objs: Array[? <: Object]): Unit                          = info(s"$msg ${objs.mkString(", ")}")
+  def info(msg: String, throwable: Throwable): Unit                              = info(s"$msg ${throwable.getMessage}")
+  def info(x$0: org.slf4j.Marker, msg: String): Unit                             = info(msg)
+  def info(x$0: org.slf4j.Marker, msg: String, obj: Object): Unit                = info(s"$msg, $obj")
+  def info(x$0: org.slf4j.Marker, msg: String, obj1: Object, obj2: Object): Unit = info(s"$msg, $obj1, $obj2")
+  def info(x$0: org.slf4j.Marker, msg: String, objs: Array[? <: Object]): Unit   = info(s"$msg ${objs.mkString(", ")}")
+  def info(x$0: org.slf4j.Marker, msg: String, throwable: Throwable): Unit       = info(s"$msg ${throwable.getMessage}")
 
   def trace(msg: String, obj: Object): Unit                        = trace(s"$msg, $obj")
   def trace(msg: String, obj1: Object, obj2: Object): Unit         = trace(s"$msg, $obj1, $obj2")
-  def trace(msg: String, objs: Array[? <: Object]): Unit           = trace(s"$msg, ${objs.mkString(", ")}")
-  def trace(msg: String, throwable: Throwable): Unit               = trace(s"$msg, ${throwable.getMessage}")
-  def trace(x$0: org.slf4j.Marker, msg: String): Unit              = ???
-  def trace(x$0: org.slf4j.Marker, msg: String, x$2: Object): Unit = ???
-  def trace(x$0: org.slf4j.Marker, msg: String, x$2: Object, x$3: Object): Unit = ???
-  def trace(x$0: org.slf4j.Marker, msg: String, x$2: Array[? <: Object]): Unit  = ???
-  def trace(x$0: org.slf4j.Marker, msg: String, x$2: Throwable): Unit           = ???
+  def trace(msg: String, objs: Array[? <: Object]): Unit           = trace(s"$msg ${objs.mkString(", ")}")
+  def trace(msg: String, throwable: Throwable): Unit               = trace(s"$msg ${throwable.getMessage}")
+  def trace(x$0: org.slf4j.Marker, msg: String): Unit              = trace(msg)
+  def trace(x$0: org.slf4j.Marker, msg: String, obj: Object): Unit = trace(s"$msg, $obj")
+  def trace(x$0: org.slf4j.Marker, msg: String, obj1: Object, obj2: Object): Unit = trace(s"$msg, $obj1, $obj2")
+  def trace(x$0: org.slf4j.Marker, msg: String, objs: Array[? <: Object]): Unit = trace(s"$msg ${objs.mkString(", ")}")
+  def trace(x$0: org.slf4j.Marker, msg: String, throwable: Throwable): Unit     = trace(s"$msg ${throwable.getMessage}")
 
-  def warn(msg: String, obj: Object): Unit                                     = warn(s"$msg, $obj")
-  def warn(msg: String, obj1: Object, obj2: Object): Unit                      = warn(s"$msg, $obj1, $obj2")
-  def warn(msg: String, objs: Array[? <: Object]): Unit                        = warn(s"$msg, ${objs.mkString(", ")}")
-  def warn(msg: String, throwable: Throwable): Unit                            = warn(s"$msg, ${throwable.getMessage}")
-  def warn(x$0: org.slf4j.Marker, msg: String): Unit                           = ???
-  def warn(x$0: org.slf4j.Marker, msg: String, x$2: Object): Unit              = ???
-  def warn(x$0: org.slf4j.Marker, msg: String, x$2: Object, x$3: Object): Unit = ???
-  def warn(x$0: org.slf4j.Marker, msg: String, x$2: Array[? <: Object]): Unit  = ???
-  def warn(x$0: org.slf4j.Marker, msg: String, x$2: Throwable): Unit           = ???
+  def warn(msg: String, obj: Object): Unit                                       = warn(s"$msg, $obj")
+  def warn(msg: String, obj1: Object, obj2: Object): Unit                        = warn(s"$msg, $obj1, $obj2")
+  def warn(msg: String, objs: Array[? <: Object]): Unit                          = warn(s"$msg ${objs.mkString(", ")}")
+  def warn(msg: String, throwable: Throwable): Unit                              = warn(s"$msg ${throwable.getMessage}")
+  def warn(x$0: org.slf4j.Marker, msg: String): Unit                             = warn(msg)
+  def warn(x$0: org.slf4j.Marker, msg: String, obj: Object): Unit               = warn(s"$msg, $obj")
+  def warn(x$0: org.slf4j.Marker, msg: String, obj1: Object, obj2: Object): Unit = warn(s"$msg, $obj1, $obj2")
+  def warn(x$0: org.slf4j.Marker, msg: String, objs: Array[? <: Object]): Unit   = warn(s"$msg ${objs.mkString(", ")}")
+  def warn(x$0: org.slf4j.Marker, msg: String, throwable: Throwable): Unit       = warn(s"$msg ${throwable.getMessage}")
+
+  /** Since woof allows much more complex filters, the best we can do is probe the filter with the requested log level
+    * and arbitrary values in the rest of the log line.
+    */
+  private def testLevel(logLevel: LogLevel): Boolean =
+    val mockLogLine = LogLine(logLevel, LogInfo("", new File("."), -1), "", Nil)
+    logger.exists(_.filter(mockLogLine))
+
+  def isDebugEnabled(): Boolean = testLevel(LogLevel.Debug)
+  def isErrorEnabled(): Boolean = testLevel(LogLevel.Error)
+  def isInfoEnabled(): Boolean  = testLevel(LogLevel.Info)
+  def isTraceEnabled(): Boolean = testLevel(LogLevel.Trace)
+  def isWarnEnabled(): Boolean  = testLevel(LogLevel.Warn)
+
+  def isInfoEnabled(x$0: org.slf4j.Marker): Boolean  = isInfoEnabled()
+  def isTraceEnabled(x$0: org.slf4j.Marker): Boolean = isTraceEnabled()
+  def isDebugEnabled(x$0: org.slf4j.Marker): Boolean = isDebugEnabled()
+  def isErrorEnabled(x$0: org.slf4j.Marker): Boolean = isErrorEnabled()
+  def isWarnEnabled(x$0: org.slf4j.Marker): Boolean  = isWarnEnabled()
 
 end WoofLogger
 
--- modules/slf4j/src/test/scala/woof/slf4j/Slf4jSuite.scala
+++ modules/slf4j/src/test/scala/woof/slf4j/Slf4jSuite.scala
@@ -13,18 +13,41 @@ import cats.effect.kernel.Clock
 class Slf4jSuite extends munit.CatsEffectSuite:
 
   override def munitTimeout = 10.minutes
+
   test("should log stuff") {
     given Printer   = NoColorPrinter()
     given Filter    = Filter.everything
     given Clock[IO] = leetClock
     for
       stringOutput <- newStringWriter
-      logger       <- Logger.makeIoLogger(stringOutput)
-      _            <- IO.delay(WoofLogger.logger = Some(logger))
+      woofLogger   <- Logger.makeIoLogger(stringOutput)
+      _            <- IO.delay(WoofLogger.logger = Some(woofLogger))
       slf4jLogger  <- IO.delay(LoggerFactory.getLogger(this.getClass))
       _            <- IO.delay(slf4jLogger.info("HELLO, SLF4J!"))
       result       <- stringOutput.get
-    yield assertEquals(result, "1987-05-31 13:37:00 [INFO ] woof.slf4j.Slf4jSuite: HELLO, SLF4J! (Slf4jSuite.scala:25)\n")
+    yield assertEquals(
+      result,
+      "1987-05-31 13:37:00 [INFO ] woof.slf4j.Slf4jSuite: HELLO, SLF4J! (Slf4jSuite.scala:26)\n",
+    )
+    end for
+  }
+
+  test("should log arrays of objects") {
+    given Printer   = NoColorPrinter()
+    given Filter    = Filter.everything
+    given Clock[IO] = leetClock
+    for
+      stringOutput <- newStringWriter
+      woofLogger   <- Logger.makeIoLogger(stringOutput)
+      _            <- IO.delay(WoofLogger.logger = Some(woofLogger))
+      slf4jLogger  <- IO.delay(LoggerFactory.getLogger(this.getClass))
+      _            <- IO.delay(slf4jLogger.info("HELLO, ARRAYS!", 1, Some(42), List(1337)))
+      result       <- stringOutput.get
+    yield assertEquals(
+      result,
+      "1987-05-31 13:37:00 [INFO ] woof.slf4j.Slf4jSuite: HELLO, ARRAYS! 1, Some(42), List(1337) (Slf4jSuite.scala:44)\n",
+    )
+    end for
   }
 
   test("should check log levels") {
