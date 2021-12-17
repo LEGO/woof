f2286f749891e515191817a5de075d32f825aaed
Felix <felix.palludan.hargreaves@lego.com>
2021-12-17 12:26:58 +0100

    Revert testFormatTime to fit w. copenhagen timezone

--- modules/core/js/src/test/scala/org/legogroup/woof/TestFormatTime.scala
+++ modules/core/js/src/test/scala/org/legogroup/woof/TestFormatTime.scala
@@ -1,3 +1,8 @@
 package org.legogroup.woof
 
-val testFormatTime = defaultTimeFormat
+import scala.scalajs.js.Date
+
+val testFormatTime = (e: EpochMillis) =>
+  val twoHours = 1000 * 60 * 60 * 2 // copenhagen offset
+  val date     = new Date(e.millis.toDouble + twoHours)
+  date.toISOString().replace("T", " ").take(19)
