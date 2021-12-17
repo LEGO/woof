8dd3f9b381ea2e58fb7c20136c5cc17de94d5ff8
Felix <felix.palludan.hargreaves@lego.com>
2021-12-17 12:18:29 +0100

    Fix bug where scalajs build would not use timezone in the browser

--- modules/core/js/src/test/scala/org/legogroup/woof/TestFormatTime.scala
+++ modules/core/js/src/test/scala/org/legogroup/woof/TestFormatTime.scala
@@ -1,8 +1,3 @@
 package org.legogroup.woof
 
-import scala.scalajs.js.Date
-
-val testFormatTime = (e: EpochMillis) =>
-  val twoHours = 1000 * 60 * 60 * 2 // copenhagen offset
-  val date     = new Date(e.millis.toDouble + twoHours)
-  date.toISOString().replace("T", " ").take(19)
+val testFormatTime = defaultTimeFormat
