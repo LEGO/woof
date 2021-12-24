4c9d21c9e5d9f2a888f98d8ba25723031c8ebab6
Scala Steward <me@scala-steward.org>
2021-12-24 02:05:25 +0100

    Reformat with scalafmt 3.2.2

--- modules/core/shared/src/main/scala/org/legogroup/woof/ColorPrinter.scala
+++ modules/core/shared/src/main/scala/org/legogroup/woof/ColorPrinter.scala
@@ -81,36 +81,36 @@ object ColorPrinter:
 
     enum Style(val code: String):
       override def toString = code
-      case Bold extends Style(Console.BOLD)
+      case Bold       extends Style(Console.BOLD)
       case Underlined extends Style(Console.UNDERLINED)
-      case Reversed extends Style(Console.REVERSED)
-      case Invisible extends Style(Console.INVISIBLE)
-      case Reset extends Style(Console.RESET)
+      case Reversed   extends Style(Console.REVERSED)
+      case Invisible  extends Style(Console.INVISIBLE)
+      case Reset      extends Style(Console.RESET)
     end Style
 
     enum Foreground(val code: String):
       override def toString = code
-      case Black extends Foreground(Console.BLACK)
-      case Green extends Foreground(Console.GREEN)
-      case Yellow extends Foreground(Console.YELLOW)
-      case Red extends Foreground(Console.RED)
-      case Bold extends Foreground(Console.BOLD)
-      case Blue extends Foreground(Console.BLUE)
+      case Black   extends Foreground(Console.BLACK)
+      case Green   extends Foreground(Console.GREEN)
+      case Yellow  extends Foreground(Console.YELLOW)
+      case Red     extends Foreground(Console.RED)
+      case Bold    extends Foreground(Console.BOLD)
+      case Blue    extends Foreground(Console.BLUE)
       case Magenta extends Foreground(Console.MAGENTA)
-      case Cyan extends Foreground(Console.CYAN)
-      case White extends Foreground(Console.WHITE)
+      case Cyan    extends Foreground(Console.CYAN)
+      case White   extends Foreground(Console.WHITE)
     end Foreground
 
     enum Background(val code: String):
       override def toString = code
-      case Black extends Background(Console.BLACK_B)
-      case Red extends Background(Console.RED_B)
-      case Green extends Background(Console.GREEN_B)
-      case Yellow extends Background(Console.YELLOW_B)
-      case Blue extends Background(Console.BLUE_B)
+      case Black   extends Background(Console.BLACK_B)
+      case Red     extends Background(Console.RED_B)
+      case Green   extends Background(Console.GREEN_B)
+      case Yellow  extends Background(Console.YELLOW_B)
+      case Blue    extends Background(Console.BLUE_B)
       case Magenta extends Background(Console.MAGENTA_B)
-      case Cyan extends Background(Console.CYAN_B)
-      case White extends Background(Console.WHITE_B)
+      case Cyan    extends Background(Console.CYAN_B)
+      case White   extends Background(Console.WHITE_B)
     end Background
   end Theme
 
