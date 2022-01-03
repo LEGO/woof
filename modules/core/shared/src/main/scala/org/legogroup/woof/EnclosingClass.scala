5e8fbbdcbab4932fb58f7f9e7dd89c3385ecec85
Isaac Karrer <isaac.karrer@elastic.co>
2022-01-03 14:36:03 -0600

    ensure termination on last possible chomp

--- modules/core/shared/src/main/scala/org/legogroup/woof/EnclosingClass.scala
+++ modules/core/shared/src/main/scala/org/legogroup/woof/EnclosingClass.scala
@@ -8,8 +8,11 @@ case class EnclosingClass(fullName: String):
 
   @tailrec
   private def reduceNameLength(name: String, chomps: Int = 0): String = {
-    if(name.length > lineLength) {
-      val names = name.split('.')
+    val names = name.split('.')
+    val isTooLong = name.length > lineLength
+    val notLastPossibleChomp = chomps < names.length
+    val keepChomping = notLastPossibleChomp && isTooLong
+    if (keepChomping) {
       names.update(chomps, names(chomps).head.toString)
       reduceNameLength(names.mkString("."), chomps + 1)
     } else {
