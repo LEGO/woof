9d3006e03be28f9a934633ddc6a07f306cfe82bc
Isaac Karrer <isaac.karrer@elastic.co>
2022-01-04 09:01:58 -0600

    Update modules/core/shared/src/main/scala/org/legogroup/woof/EnclosingClass.scala
    Co-authored-by: Felix Bjært Hargreaves <hejfelix@users.noreply.github.com>

--- modules/core/shared/src/main/scala/org/legogroup/woof/EnclosingClass.scala
+++ modules/core/shared/src/main/scala/org/legogroup/woof/EnclosingClass.scala
@@ -6,15 +6,13 @@ case class EnclosingClass(fullName: String, lineLength: Int = 80):
   def printableName: String = reduceNameLength(fullName)
 
   @tailrec
-  private def reduceNameLength(name: String, chomps: Int = 0): String = {
-    val names = name.split('.')
-    val isTooLong = name.length > lineLength
-    val notLastPossibleChomp = chomps < names.length - 1
-    val keepChomping = notLastPossibleChomp && isTooLong
-    if (keepChomping) {
-      names.update(chomps, names(chomps).head.toString)
-      reduceNameLength(names.mkString("."), chomps + 1)
-    } else {
-      name
-    }
+  private def reduceNameLength(name: String): String =
+    List
+      .unfold(name.split('.').toList) {
+        case Nil                                                   => none
+        case last :: Nil                                           => (last, Nil).some
+        case parts @ x :: xs if parts.mkString.length > lineLength => (x.take(1), xs).some
+        case x :: xs                                               => (x, xs).some
+      }
+      .mkString(".")
   }
