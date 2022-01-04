ec800651091bc20b7a674d2e3e6cc3a374fb21bd
Isaac Karrer <isaac.karrer@elastic.co>
2022-01-04 09:10:27 -0600

    Update modules/core/shared/src/test/scala/org/legogroup/woof/EnclosingClassSuite.scala
    Co-authored-by: Felix Bjært Hargreaves <hejfelix@users.noreply.github.com>

--- modules/core/shared/src/test/scala/org/legogroup/woof/EnclosingClassSuite.scala
+++ modules/core/shared/src/test/scala/org/legogroup/woof/EnclosingClassSuite.scala
@@ -46,6 +46,8 @@ class EnclosingClassSuite extends CatsEffectSuite:
   test("does not abbreviate package if too short") {
     assertEquals(EnclosingClass("Too.$hort", 10).printableName, "Too.$hort")
   }
-
+  test("partially chomped") {
+    assertEquals(EnclosingClass("never.gonna.give.you.up", 10).printableName, "n.g.give.you.up")
+  }
 end EnclosingClassSuite
 
