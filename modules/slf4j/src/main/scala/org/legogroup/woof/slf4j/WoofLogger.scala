23eb088b300907279266ce018a16f58a74a77c04
Felix <felix.palludan.hargreaves@lego.com>
2022-01-13 09:58:36 +0100

    set `trailingCommas` to `keep` to prevent sclafmt bug.

--- modules/slf4j/src/main/scala/org/legogroup/woof/slf4j/WoofLogger.scala
+++ modules/slf4j/src/main/scala/org/legogroup/woof/slf4j/WoofLogger.scala
@@ -14,9 +14,10 @@ class WoofLogger(name: String) extends Logger:
 
   private def getLogInfo() =
     val stacktraceElements = Thread.currentThread().getStackTrace()
-    val callingMethodIndex =
-      stacktraceElements.size - stacktraceElements.reverse.indexWhere(s => s.getClassName == this.getClass.getName,
-      ) // after last mention of this class
+    val lastIndex = stacktraceElements.reverse.indexWhere(s =>
+      s.getClassName == this.getClass.getName
+    ) // after last mention of this class
+    val callingMethodIndex               = stacktraceElements.size - lastIndex
     val callingMethod: StackTraceElement = stacktraceElements(callingMethodIndex)
     val enclosingClassName               = EnclosingClass(callingMethod.getClassName)
     val fileName   = (enclosingClassName.fullName.replace('.', '/') + ".scala").split("\\/").takeRight(1).mkString
