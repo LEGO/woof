c28da69ac84aaf5ec19f4b202eafca3db56ab3db
Karsten Heino Karlsen <karsten.heino.karlsen@lego.com>
2022-05-19 16:16:25 +0200

    Add examples/FileOutput.scala

---
+++ modules/examples/src/main/scala/examples/FileOutput.scala
@@ -0,0 +1,26 @@
+package examples
+
+import cats.effect.{IO, IOApp, Resource}
+import java.io.{PrintWriter, FileWriter}
+import org.legogroup.woof.{*, given}
+
+object FileOutput extends IOApp.Simple:
+
+  given Filter  = Filter.everything
+  given Printer = NoColorPrinter()
+
+  val fileOutput =
+    def writeLine(line: String, logPath: String) =
+      val writer = IO(PrintWriter(FileWriter(logPath, true)))
+      val res = Resource.make(writer) { w => IO(w.close) }
+      res.use { w => IO(w.println(line)) }
+    new Output[IO]:
+      def output(str: String)      = writeLine(str, "woof.log")
+      def outputError(str: String) = writeLine(str, "woof.err")
+
+  def run =
+    for
+      given Logger[IO] <- DefaultLogger.makeIo(fileOutput, Output.fromConsole)
+      _                <- Logger[IO].info("Hello, Info!")
+      _                <- Logger[IO].error("Hello, Error!")
+    yield ()
