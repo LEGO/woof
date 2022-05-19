65fc1df290ec6a91e60b776850858065f52ef6ab
Felix <felix.palludan.hargreaves@lego.com>
2022-05-19 14:00:08 +0200

    Add scalajs hello world

---
+++ modules/examples-scalajs/src/main/scala/examples/HelloScalaJs.scala
@@ -0,0 +1,24 @@
+package examples
+
+import cats.effect.{IO, IOApp}
+import org.legogroup.woof.{*, given}
+
+
+/**
+ * To run this, open sbt and call:
+ * `examplesJs/fastOptJS`
+ * Then, open `modules/examples-scalajs/index.html` in your browser 
+ * and look at the console
+ */
+object HelloScalaJs extends IOApp.Simple:
+
+  given Filter  = Filter.everything
+  given Printer = NoColorPrinter()
+  
+  def run =
+    for
+      given Logger[IO] <- DefaultLogger.makeIo(Output.fromConsole)
+      _                <- Logger[IO].info("Hello, Scala.Js!")
+    yield ()
+
+end HelloScalaJs
