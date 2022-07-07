960cee9d458d55f9ae4f33d6e29564c923611520
Scala Steward <scala_steward@virtuslab.com>
2022-07-07 21:32:53 +0000

    Reformat with scalafmt 3.5.8
    Executed command: scalafmt --non-interactive

--- modules/examples-scalajs/src/main/scala/examples/HelloScalaJs.scala
+++ modules/examples-scalajs/src/main/scala/examples/HelloScalaJs.scala
@@ -3,18 +3,14 @@ package examples
 import cats.effect.{IO, IOApp}
 import org.legogroup.woof.{*, given}
 
-
-/**
- * To run this, open sbt and call:
- * `examplesJs/fastOptJS`
- * Then, open `modules/examples-scalajs/index.html` in your browser 
- * and look at the console
- */
+/** To run this, open sbt and call: `examplesJs/fastOptJS` Then, open `modules/examples-scalajs/index.html` in your
+  * browser and look at the console
+  */
 object HelloScalaJs extends IOApp.Simple:
 
   given Filter  = Filter.everything
   given Printer = NoColorPrinter()
-  
+
   def run =
     for
       given Logger[IO] <- DefaultLogger.makeIo(Output.fromConsole)
